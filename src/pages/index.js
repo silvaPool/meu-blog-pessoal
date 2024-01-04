2ae5ccc2cbb82432d0e412dafebf6ff6b737419b
silvaPool <silva.friends22@gmail.com>
2024-01-04 15:05:35 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // import { Link } from 'gatsby'
 
 const IndexPage = () => {
@@ -12,6 +13,10 @@ const IndexPage = () => {
 
     <Layout pageTitle="Home Page">
       <p>Meu blog pessoal</p>
+      <StaticImage
+      alt="Imagem de uma cruz"
+      src="../images/cruzblog.jpg"
+      />
     </Layout>
   )
 }
