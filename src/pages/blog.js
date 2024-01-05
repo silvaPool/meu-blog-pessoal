97cee928a44198619b140fee1a743a4ab373fa88
silvaPool <silva.friends22@gmail.com>
2024-01-05 16:04:04 -0300

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,26 +7,33 @@ const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
             <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
+            {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      }
             </ul>
         </Layout>
     )
 }
 
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
+  }
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
